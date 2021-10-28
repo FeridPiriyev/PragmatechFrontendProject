9950174f06b5b7466894238758ceef57de9deefb
ferid <smdpiriyev@gmail.com>
2021-10-28 20:36:16 +0400

    Js practice

---
+++ JavaScript/Week05-Day02/function.js
@@ -0,0 +1,7 @@
+let names=['Zohrab','Qurbani','Iskender','Nezrin','Ferid','Huseyn']
+
+// daxilinde o hərfi olan tələbələrin adlarını bir arraya yigin
+//1. o herfini tapmaq sozun icerisinde
+//2. serte uygun sozu arraya elave etmek
+// adinin uzunlugu 5-dən boyuk olan tələbələrin adlarini bir arrya yigin
+// tələbə adlarinin boyuk herfle yazilmis versiyasinin oldugu array yaradin
