013515991481910e287fcec4d74840a5c38f1536
FeridPiriyev <smdpiriyev@gmail.com>
2022-01-26 15:05:27 +0400

    sehifeler arasinda kecid react-router-dom

---
+++ React/HomeWork/3.1/src/components/Arrow.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useNavigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./Home";
+
+
+export default function Arrow() {
+    const navigate = useNavigate()
+    return <div>
+        <button onClick={() => {
+            navigate({
+                pathname: "/"
+            })
+        }}>Kecid</button>
+
+    </div>;
+}
+
